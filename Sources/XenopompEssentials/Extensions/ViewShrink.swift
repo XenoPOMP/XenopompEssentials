c27fb1cf6199aaa0591fe42ff50bc33bf18c637c
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-05-25 10:04:15 +0200

    🧪 update ViewShrink extensions

--- Sources/XenopompEssentials/Extensions/ViewShrink.swift
+++ Sources/XenopompEssentials/Extensions/ViewShrink.swift
@@ -13,17 +13,18 @@ public enum Dimensions {
 
 struct Shrink: ViewModifier {
     var dimension: Dimensions
+    var alignment: Alignment = .center
     
     func body(content: Content) -> some View {
         switch self.dimension {
             case .width:
-                return content.frame(maxWidth: .infinity)
+                return content.frame(maxWidth: .infinity, alignment: self.alignment)
             
             case .height:
-                return content.frame(maxHeight: .infinity)
+                return content.frame(maxHeight: .infinity, alignment: self.alignment)
             
             case .both:
-                return content.frame(maxWidth: .infinity, maxHeight: .infinity)
+                return content.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: self.alignment)
         }
     }
 }
@@ -33,10 +34,33 @@ extension View {
     public func shrink(at dimension: Dimensions) -> some View {
         modifier(Shrink(dimension: dimension))
     }
+    
+    /// Shrinks view to whole width/height/both dimensions and aligns it.
+    public func shrink(at dimension: Dimensions, align: Alignment) -> some View {
+        modifier(Shrink(dimension: dimension, alignment: align))
+    }
 }
 
 #Preview {
-    Text("Shrinked!")
-        .shrink(at: .width)
-        .background(Color.red)
+    ScrollView {
+        VStack(spacing: 10) {
+            Text("Shrinked to .leading!")
+                .shrink(at: .width, align: .leading)
+                .padding(20)
+                .background(Color.red)
+                .clipShape(.rect(cornerRadius: 12))
+            
+            Text("Shrinked to .center!")
+                .shrink(at: .width, align: .center)
+                .padding(20)
+                .background(Color.red)
+                .clipShape(.rect(cornerRadius: 12))
+            
+            Text("Shrinked to .trailing!")
+                .shrink(at: .width, align: .trailing)
+                .padding(20)
+                .background(Color.red)
+                .clipShape(.rect(cornerRadius: 12))
+        }
+    }
 }
